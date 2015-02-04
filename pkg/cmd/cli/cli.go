6be4072985044119f86a885648a02cc0fd20f299
fabianofranz <contact@fabianofranz.com>
2015-02-04 19:36:26 -0200

    Custom template for cli and osc, hide global options

--- pkg/cmd/cli/cli.go
+++ pkg/cmd/cli/cli.go
@@ -10,6 +10,7 @@ import (
 	"github.com/spf13/pflag"
 
 	"github.com/openshift/origin/pkg/cmd/cli/cmd"
+	"github.com/openshift/origin/pkg/cmd/templates"
 )
 
 const longDesc = `
@@ -49,6 +50,10 @@ func NewCommandCLI(name, fullName string) *cobra.Command {
 	f := cmd.NewFactory(clientConfig)
 	f.BindFlags(cmds.PersistentFlags())
 	out := os.Stdout
+
+	cmds.SetUsageTemplate(templates.CliUsageTemplate)
+	cmds.SetHelpTemplate(templates.CliHelpTemplate)
+
 	// Kubernetes CRUD commands
 	cmds.AddCommand(f.NewCmdGet(out))
 	cmds.AddCommand(f.NewCmdDescribe(out))
