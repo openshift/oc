379a5a786709edf8a586abc62e421812a5d14adf
David Eads <deads@redhat.com>
2018-07-23 15:03:51 -0400

    remove build helpers from api packages

--- pkg/oc/lib/buildapihelpers/sort_test.go
+++ pkg/oc/lib/buildapihelpers/sort_test.go
@@ -1,9 +1,11 @@
-package build
+package buildapihelpers
 
 import (
 	"sort"
 	"testing"
 	"time"
+
+	buildinternalapi "github.com/openshift/origin/pkg/build/apis/build"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -11,7 +13,7 @@ import (
 func TestSortBuildSliceByCreationTimestamp(t *testing.T) {
 	present := metav1.Now()
 	past := metav1.NewTime(present.Add(-time.Minute))
-	builds := []Build{
+	builds := []buildinternalapi.Build{
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:              "present",
@@ -34,7 +36,7 @@ func TestSortBuildSliceByCreationTimestamp(t *testing.T) {
 func TestSortBuildPtrSliceByCreationTimestamp(t *testing.T) {
 	present := metav1.Now()
 	past := metav1.NewTime(present.Add(-time.Minute))
-	builds := []*Build{
+	builds := []*buildinternalapi.Build{
 		{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:              "present",
