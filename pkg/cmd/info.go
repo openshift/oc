320597c1a5f69cffbd50508cb0cc9db5e2f29623
juanvallejo <jvallejo@redhat.com>
2019-01-09 16:25:14 -0500

    add config.openshift.io resource gathering

--- pkg/cmd/info.go
+++ pkg/cmd/info.go
@@ -2,11 +2,21 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path"
 
 	"github.com/spf13/cobra"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/cli-runtime/pkg/genericclioptions/printers"
 	"k8s.io/cli-runtime/pkg/genericclioptions/resource"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
+
+	"io/ioutil"
 
 	configv1 "github.com/openshift/api/config/v1"
 )
@@ -22,16 +32,27 @@ var (
 )
 
 type InfoOptions struct {
+	printFlags  *genericclioptions.PrintFlags
 	configFlags *genericclioptions.ConfigFlags
 
+	discoveryClient discovery.CachedDiscoveryInterface
+	dynamicClient   dynamic.Interface
+
+	printer printers.ResourcePrinter
 	builder *resource.Builder
 	args    []string
+
+	// directory where all gathered data will be stored
+	baseDir string
+	// whether or not to allow writes to an existing and populated base directory
+	overwrite bool
 
 	genericclioptions.IOStreams
 }
 
 func NewInfoOptions(streams genericclioptions.IOStreams) *InfoOptions {
 	return &InfoOptions{
+		printFlags:  genericclioptions.NewPrintFlags("gathered").WithDefaultOutput("yaml"),
 		configFlags: genericclioptions.NewConfigFlags(),
 		IOStreams:   streams,
 	}
@@ -60,21 +81,46 @@ func NewCmdInfo(parentName string, streams genericclioptions.IOStreams) *cobra.C
 		},
 	}
 
+	cmd.Flags().StringVar(&o.baseDir, "base-dir", "must-gather", "Root directory used for storing all gathered cluster operator data. Defaults to $(PWD)/must-gather")
+	cmd.Flags().BoolVar(&o.overwrite, "overwrite", false, "If true, allow this command to write to an existing location with previous data present")
+
+	o.printFlags.AddFlags(cmd)
 	return cmd
 }
 
 func (o *InfoOptions) Complete(cmd *cobra.Command, args []string) error {
 	o.args = args
 
-	o.configFlags.ToRESTConfig()
-	o.builder = resource.NewBuilder(o.configFlags)
+	config, err := o.configFlags.ToRESTConfig()
+	if err != nil {
+		return err
+	}
 
+	o.dynamicClient, err = dynamic.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+
+	o.discoveryClient, err = o.configFlags.ToDiscoveryClient()
+	if err != nil {
+		return err
+	}
+
+	o.printer, err = o.printFlags.ToPrinter()
+	if err != nil {
+		return err
+	}
+
+	o.builder = resource.NewBuilder(o.configFlags)
 	return nil
 }
 
 func (o *InfoOptions) Validate() error {
 	if len(o.args) != 1 {
 		return fmt.Errorf("exactly 1 argument (operator name) is supported")
+	}
+	if len(o.baseDir) == 0 {
+		return fmt.Errorf("--base-dir must not be empty")
 	}
 	return nil
 }
@@ -91,7 +137,18 @@ func (o *InfoOptions) Run() error {
 		return err
 	}
 
+	if err := o.ensureDirectoryViable(o.baseDir, o.overwrite); err != nil {
+		return err
+	}
+
+	// first, gather config.openshift.io resource data
+	if err := o.gatherConfigResourceData(path.Join(o.baseDir, "/resources/config.openshift.io")); err != nil {
+		// TODO: aggregate error
+		return err
+	}
+
 	for _, info := range infos {
+		// TODO: aggregate errors
 		if configv1.GroupName != info.Mapping.GroupVersionKind.Group {
 			return fmt.Errorf("unexpected resource API group %q. Expected %q", info.Mapping.GroupVersionKind.Group, configv1.GroupName)
 		}
@@ -99,7 +156,132 @@ func (o *InfoOptions) Run() error {
 			return fmt.Errorf("unsupported resource type, must be %q", "clusteroperators")
 		}
 
+		// save clusteroperator resources
+		if err := o.gatherClusterOperatorResource(path.Join(o.baseDir, "/resources"), info); err != nil {
+			return err
+		}
+
 	}
 
 	return nil
+}
+
+// ensureDirectoryViable returns an error if the given path:
+// 1. already exists AND is a file (not a directory)
+// 2. already exists AND is NOT empty
+// 3. an IO error occurs
+func (o *InfoOptions) ensureDirectoryViable(dirPath string, allowDataOverride bool) error {
+	baseDirInfo, err := os.Stat(dirPath)
+	if err != nil && os.IsNotExist(err) {
+		// no error, directory simply does not exist yet
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+
+	if !baseDirInfo.IsDir() {
+		return fmt.Errorf("%q exists and is a file", dirPath)
+	}
+	files, err := ioutil.ReadDir(dirPath)
+	if err != nil {
+		return err
+	}
+	if len(files) > 0 && !o.overwrite {
+		return fmt.Errorf("%q exists and is not empty. Pass --overwrite to allow data overwrites", dirPath)
+	}
+	return nil
+}
+
+func (o *InfoOptions) gatherClusterOperatorResource(destDir string, info *resource.Info) error {
+	// ensure destination path exists
+	if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	filename := fmt.Sprintf("%s.yaml", info.Name)
+	dest, err := os.OpenFile(path.Join(destDir, "/"+filename), os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	if err := o.printer.PrintObj(info.Object, dest); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (o *InfoOptions) gatherConfigResourceData(destDir string) error {
+	// ensure destination path exists
+	if err := os.MkdirAll(destDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	resources, err := retrieveConfigResourceNames(o.discoveryClient)
+	if err != nil {
+		return err
+	}
+
+	for _, resource := range resources {
+		resourceList, err := o.dynamicClient.Resource(resource).List(metav1.ListOptions{})
+		if err != nil {
+			// TODO: aggregate errors, do not fail on a single one in order to collect some metrics despite failures
+			return err
+		}
+
+		objToPrint := runtime.Object(resourceList)
+
+		err = func() error {
+			filename := fmt.Sprintf("%s.yaml", resource.Resource)
+			dest, err := os.OpenFile(path.Join(destDir, "/"+filename), os.O_RDWR|os.O_CREATE, 0755)
+			if err != nil {
+				return err
+			}
+			defer dest.Close()
+
+			if err := o.printer.PrintObj(objToPrint, dest); err != nil {
+				return err
+			}
+
+			return nil
+		}()
+		if err != nil {
+			// TODO: aggregate this error
+			return err
+		}
+	}
+
+	return nil
+}
+
+func retrieveConfigResourceNames(discoveryClient discovery.CachedDiscoveryInterface) ([]schema.GroupVersionResource, error) {
+	lists, err := discoveryClient.ServerPreferredResources()
+	if err != nil {
+		return nil, err
+	}
+
+	resources := []schema.GroupVersionResource{}
+	for _, list := range lists {
+		if len(list.APIResources) == 0 {
+			continue
+		}
+		gv, err := schema.ParseGroupVersion(list.GroupVersion)
+		if err != nil {
+			continue
+		}
+		for _, resource := range list.APIResources {
+			if len(resource.Verbs) == 0 {
+				continue
+			}
+			// filter groups outside of config.openshift.io
+			if gv.Group != configv1.GroupName {
+				continue
+			}
+			resources = append(resources, schema.GroupVersionResource{Group: gv.Group, Version: gv.Version, Resource: resource.Name})
+		}
+	}
+
+	return resources, nil
 }
