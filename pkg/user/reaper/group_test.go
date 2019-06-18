openshift/origin

fix-test: misc unit tests
- split origin/kube objects for fake client (which have their own scheme each)
- pkg/deploy/api/validation/validation_test.go error output
- pkg/security test strings
- pkg/image/api/validation/validation_test.go

pkg/user/reaper/group_test.go
@@ -12,6 +12,7 @@ import (
 	"k8s.io/kubernetes/pkg/client/clientset_generated/internalclientset/fake"
 
 	"github.com/davecgh/go-spew/spew"
+	oapi "github.com/openshift/origin/pkg/api"
 	authorizationapi "github.com/openshift/origin/pkg/authorization/api"
 	"github.com/openshift/origin/pkg/client/testclient"
 
@@ -127,8 +128,8 @@ func TestGroupReaper(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		tc := testclient.NewSimpleFake(test.objects...)
-		ktc := fake.NewSimpleClientset(test.objects...)
+		tc := testclient.NewSimpleFake(oapi.OriginObjects(test.objects)...)
+		ktc := fake.NewSimpleClientset(oapi.UpstreamObjects(test.objects)...)
 
 		actual := []interface{}{}
 		oreactor := func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {

pkg/user/reaper/user_test.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 
+	oapi "github.com/openshift/origin/pkg/api"
 	authorizationapi "github.com/openshift/origin/pkg/authorization/api"
 	"github.com/openshift/origin/pkg/client/testclient"
 	oauthapi "github.com/openshift/origin/pkg/oauth/api"
@@ -212,8 +213,8 @@ func TestUserReaper(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		tc := testclient.NewSimpleFake(test.objects...)
-		ktc := fake.NewSimpleClientset(test.objects...)
+		tc := testclient.NewSimpleFake(oapi.OriginObjects(test.objects)...)
+		ktc := fake.NewSimpleClientset(oapi.UpstreamObjects(test.objects)...)
 
 		actual := []interface{}{}
 		oreactor := func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {

openshift/oc

pkg/user/reaper/group_test.go
@@ -127,8 +127,8 @@ func TestGroupReaper(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		tc := testclient.NewSimpleFake(test.objects...)
-		ktc := fake.NewSimpleClientset(test.objects...)
+		tc := testclient.NewSimpleFake(testclient.OriginObjects(test.objects)...)
+		ktc := fake.NewSimpleClientset(testclient.UpstreamObjects(test.objects)...)
 
 		actual := []interface{}{}
 		oreactor := func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {

pkg/user/reaper/user_test.go
@@ -212,8 +212,8 @@ func TestUserReaper(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		tc := testclient.NewSimpleFake(test.objects...)
-		ktc := fake.NewSimpleClientset(test.objects...)
+		tc := testclient.NewSimpleFake(testclient.OriginObjects(test.objects)...)
+		ktc := fake.NewSimpleClientset(testclient.UpstreamObjects(test.objects)...)
 
 		actual := []interface{}{}
 		oreactor := func(action clientgotesting.Action) (handled bool, ret runtime.Object, err error) {