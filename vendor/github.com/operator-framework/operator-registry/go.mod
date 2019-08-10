a9b06f8976264011df5e5f420f16f30f4d1c5bf6
Evan Cordell <cordell.evan@gmail.com>
2019-08-10 10:07:24 -0400

    bump(*)

---
+++ vendor/github.com/operator-framework/operator-registry/go.mod
@@ -0,0 +1,27 @@
+module github.com/operator-framework/operator-registry
+
+require (
+	github.com/antihax/optional v0.0.0-20180407024304-ca021399b1a6
+	github.com/ghodss/yaml v1.0.0
+	github.com/golang/groupcache v0.0.0-20181024230925-c65c006176ff // indirect
+	github.com/golang/mock v1.1.1
+	github.com/golang/protobuf v1.2.0
+	github.com/gregjones/httpcache v0.0.0-20190203031600-7a902570cb17 // indirect
+	github.com/grpc-ecosystem/grpc-health-probe v0.2.1-0.20181220223928-2bf0a5b182db
+	github.com/imdario/mergo v0.3.7 // indirect
+	github.com/mattn/go-sqlite3 v1.10.0
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/operator-framework/operator-lifecycle-manager v0.0.0-20190125151539-1e295784b30a
+	github.com/sirupsen/logrus v1.2.0
+	github.com/spf13/cobra v0.0.3
+	github.com/stretchr/testify v1.2.2
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a
+	golang.org/x/oauth2 v0.0.0-20181105165119-ca4130e427c7
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4 // indirect
+	golang.org/x/tools v0.0.0-20181207222222-4c874b978acb // indirect
+	google.golang.org/grpc v1.17.0
+	k8s.io/api v0.0.0-20190118113203-912cbe2bfef3
+	k8s.io/apiextensions-apiserver v0.0.0-20181204003618-e419c5771cdc
+	k8s.io/apimachinery v0.0.0-20190118094746-1525e4dadd2d
+	k8s.io/client-go v8.0.0+incompatible
+)
---
+++ vendor/github.com/operator-framework/operator-registry/pkg/sqlite/configmap.go
@@ -0,0 +1,174 @@
+package sqlite
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/ghodss/yaml"
+	"github.com/operator-framework/operator-registry/pkg/registry"
+	"github.com/sirupsen/logrus"
+	"k8s.io/api/core/v1"
+	"k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+)
+
+const (
+	ConfigMapCRDName     = "customResourceDefinitions"
+	ConfigMapCSVName     = "clusterServiceVersions"
+	ConfigMapPackageName = "packages"
+)
+
+// ConfigMapLoader loads a configmap of resources into the database
+// entries under "customResourceDefinitions" will be parsed as CRDs
+// entries under "clusterServiceVersions"  will be parsed as CSVs
+// entries under "packages" will be parsed as Packages
+type ConfigMapLoader struct {
+	log           *logrus.Entry
+	store         registry.Load
+	configMapData map[string]string
+	crds          map[registry.APIKey]*unstructured.Unstructured
+}
+
+var _ SQLPopulator = &ConfigMapLoader{}
+
+// NewSQLLoaderForConfigMapData is useful when the operator manifest(s)
+// originate from a different source than a configMap. For example, operator
+// manifest(s) can be downloaded from a remote registry like quay.io.
+func NewSQLLoaderForConfigMapData(logger *logrus.Entry, store registry.Load, configMapData map[string]string) *ConfigMapLoader {
+	return &ConfigMapLoader{
+		log:           logger,
+		store:         store,
+		configMapData: configMapData,
+		crds:          map[registry.APIKey]*unstructured.Unstructured{},
+	}
+}
+
+func NewSQLLoaderForConfigMap(store registry.Load, configMap v1.ConfigMap) *ConfigMapLoader {
+	logger := logrus.WithFields(logrus.Fields{"configmap": configMap.GetName(), "ns": configMap.GetNamespace()})
+	return &ConfigMapLoader{
+		log:           logger,
+		store:         store,
+		configMapData: configMap.Data,
+		crds:          map[registry.APIKey]*unstructured.Unstructured{},
+	}
+}
+
+func (c *ConfigMapLoader) Populate() error {
+	c.log.Info("loading CRDs")
+
+	// first load CRDs into memory; these will be added to the bundle that owns them
+	crdListYaml, ok := c.configMapData[ConfigMapCRDName]
+	if !ok {
+		return fmt.Errorf("couldn't find expected key %s in configmap", ConfigMapCRDName)
+	}
+
+	crdListJson, err := yaml.YAMLToJSON([]byte(crdListYaml))
+	if err != nil {
+		c.log.WithError(err).Debug("error loading CRD list")
+		return err
+	}
+
+	var parsedCRDList []v1beta1.CustomResourceDefinition
+	if err := json.Unmarshal(crdListJson, &parsedCRDList); err != nil {
+		c.log.WithError(err).Debug("error parsing CRD list")
+		return err
+	}
+
+	for _, crd := range parsedCRDList {
+		if crd.Spec.Versions == nil && crd.Spec.Version != "" {
+			crd.Spec.Versions = []v1beta1.CustomResourceDefinitionVersion{{Name: crd.Spec.Version, Served: true, Storage: true}}
+		}
+		for _, version := range crd.Spec.Versions {
+			gvk := registry.APIKey{Group: crd.Spec.Group, Version: version.Name, Kind: crd.Spec.Names.Kind, Plural: crd.Spec.Names.Plural}
+			c.log.WithField("gvk", gvk).Debug("loading CRD")
+			if _, ok := c.crds[gvk]; ok {
+				c.log.WithField("gvk", gvk).Debug("crd added twice")
+				return fmt.Errorf("can't add the same CRD twice in one configmap")
+			}
+			crdUnst, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&crd)
+			if err != nil {
+				c.log.WithError(err).Debug("error remarshalling crd")
+				return err
+			}
+			c.crds[gvk] = &unstructured.Unstructured{Object: crdUnst}
+		}
+	}
+
+	c.log.Info("loading Bundles")
+	csvListYaml, ok := c.configMapData[ConfigMapCSVName]
+	if !ok {
+		return fmt.Errorf("couldn't find expected key %s in configmap", ConfigMapCSVName)
+	}
+	csvListJson, err := yaml.YAMLToJSON([]byte(csvListYaml))
+	if err != nil {
+		c.log.WithError(err).Debug("error loading CSV list")
+		return err
+	}
+
+	var parsedCSVList []registry.ClusterServiceVersion
+	err = json.Unmarshal(csvListJson, &parsedCSVList)
+	if err != nil {
+		c.log.WithError(err).Debug("error parsing CSV list")
+		return err
+	}
+
+	for _, csv := range parsedCSVList {
+		c.log.WithField("csv", csv.GetName()).Debug("loading CSV")
+		csvUnst, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&csv)
+		if err != nil {
+			c.log.WithError(err).Debug("error remarshalling csv")
+			return err
+		}
+
+		bundle := registry.NewBundle(csv.GetName(), "", "", &unstructured.Unstructured{Object: csvUnst})
+		ownedCRDs, _, err := csv.GetCustomResourceDefintions()
+		if err != nil {
+			return err
+		}
+		for _, owned := range ownedCRDs {
+			split := strings.SplitN(owned.Name, ".", 2)
+			if len(split) < 2 {
+				c.log.WithError(err).Debug("error parsing owned name")
+				return fmt.Errorf("error parsing owned name")
+			}
+			gvk := registry.APIKey{Group: split[1], Version: owned.Version, Kind: owned.Kind, Plural: split[0]}
+			if crdUnst, ok := c.crds[gvk]; !ok {
+				c.log.WithField("gvk", gvk).WithError(err).Warn("couldn't find owned CRD in crd list")
+			} else {
+				bundle.Add(crdUnst)
+			}
+		}
+		if err := c.store.AddOperatorBundle(bundle); err != nil {
+			return err
+		}
+	}
+
+	c.log.Info("loading Packages")
+	packageListYaml, ok := c.configMapData[ConfigMapPackageName]
+	if !ok {
+		return fmt.Errorf("couldn't find expected key %s in configmap", ConfigMapPackageName)
+	}
+
+	packageListJson, err := yaml.YAMLToJSON([]byte(packageListYaml))
+	if err != nil {
+		c.log.WithError(err).Debug("error loading package list")
+		return err
+	}
+
+	var parsedPackageManifests []registry.PackageManifest
+	err = json.Unmarshal(packageListJson, &parsedPackageManifests)
+	if err != nil {
+		c.log.WithError(err).Debug("error parsing package list")
+		return err
+	}
+	for _, packageManifest := range parsedPackageManifests {
+		c.log.WithField("package", packageManifest.PackageName).Debug("loading package")
+		if err := c.store.AddPackageChannels(packageManifest); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
