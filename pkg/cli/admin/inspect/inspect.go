531c56384884ef69fb8c204cd10ac7a4c34963ea
Maciej Szulik <maszulik@redhat.com>
2019-10-22 22:10:17 +0200

    Wire -A and don't add printer flags

--- pkg/cli/admin/inspect/inspect.go
+++ pkg/cli/admin/inspect/inspect.go
@@ -32,7 +32,7 @@ var (
 	inspectLong = templates.LongDesc(`
 		Gather debugging information for a resource.
 
-		This command downloads the specified resource and any related 
+		This command downloads the specified resource and any related
 		resources for the purpose of gathering debugging information.
 
 		Experimental: This command is under active development and may change without notice.
@@ -106,10 +106,9 @@ func NewCmdInspect(streams genericclioptions.IOStreams, parentCommandPath string
 	}
 
 	cmd.Flags().StringVar(&o.destDir, "dest-dir", o.destDir, "Root directory used for storing all gathered cluster operator data. Defaults to $(PWD)/inspect.local.<rand>")
-	cmd.Flags().BoolVar(&o.allNamespaces, "all-namespaces", o.allNamespaces, "If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even if specified with --namespace.")
+	cmd.Flags().BoolVarP(&o.allNamespaces, "all-namespaces", "A", o.allNamespaces, "If present, list the requested object(s) across all namespaces. Namespace in current context is ignored even if specified with --namespace.")
 
 	o.configFlags.AddFlags(cmd.Flags())
-	o.printFlags.AddFlags(cmd)
 	return cmd
 }
 
