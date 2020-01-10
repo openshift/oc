514a44e81f88f00a8e4b1a63ac01e941a264ea43
Maciej Szulik <maszulik@redhat.com>
2020-01-10 16:29:14 +0100

    bump(): pin release-4.3

--- vendor/github.com/openshift/api/config/v1/types_feature.go
+++ vendor/github.com/openshift/api/config/v1/types_feature.go
@@ -103,6 +103,7 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 			"SupportPodPidsLimit",            // sig-pod, sjenning
 			"NodeDisruptionExclusion",        // sig-scheduling, ccoleman
 			"ServiceNodeExclusion",           // sig-scheduling, ccoleman
+			"SCTPSupport",                    // sig-network, ccallend
 		},
 		Disabled: []string{
 			"LegacyNodeRoleBehavior", // sig-scheduling, ccoleman
@@ -130,6 +131,7 @@ var FeatureSets = map[FeatureSet]*FeatureGateEnabledDisabled{
 			"TopologyManager",                // sig-pod, sjenning
 			"NodeDisruptionExclusion",        // sig-scheduling, ccoleman
 			"ServiceNodeExclusion",           // sig-scheduling, ccoleman
+			"SCTPSupport",                    // sig-network, ccallend
 		},
 		Disabled: []string{
 			"LegacyNodeRoleBehavior", // sig-scheduling, ccoleman
