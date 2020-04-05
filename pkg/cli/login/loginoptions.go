63bbb9e6a9fab51801eb2365e9151d5888ce563c
Clayton Coleman <ccoleman@redhat.com>
2020-04-04 22:11:45 -0400

    login: Login project list against Kube should be clearer
    We should identify that we are using the default namespace, and that
    you switch namespaces with oc project. The single quotes around the
    command are removed as that is not how we display nested commands.

--- pkg/cli/login/loginoptions.go
+++ pkg/cli/login/loginoptions.go
@@ -269,7 +269,7 @@ func (o *LoginOptions) gatherProjectInfo() error {
 	projectsList, err := projectClient.Projects().List(context.TODO(), metav1.ListOptions{})
 	// if we're running on kube (or likely kube), just set it to "default"
 	if kerrors.IsNotFound(err) || kerrors.IsForbidden(err) {
-		fmt.Fprintf(o.Out, "Using \"default\".  You can switch projects with:\n\n '%s project <projectname>'\n", o.CommandName)
+		fmt.Fprintf(o.Out, "Using \"default\" namespace.  You can switch namespaces with:\n\n %s project <projectname>\n", o.CommandName)
 		o.Project = "default"
 		return nil
 	}
