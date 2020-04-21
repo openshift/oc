65e1a60c55f4e2a7eb496ecfc5fc198b2d6feaa3
W. Trevor King <wking@tremily.us>
2020-04-21 14:52:59 -0700

    pkg/cli/admin/upgrade: Soften --to-image warning
    The warning is from cd30f2f864 (Add `oc adm upgrade` to display
    available updates or trigger an update, 2018-12-04), but the "does not
    check for upgrade compatibility..." portion is stale since 0501d04ff1
    (upgrade: Separate flags for safety instead of abusing force,
    2019-09-27, #109) added --allow-explicit-upgrade.
    
    Also fixes an unrelated "next available" -> "latest available" for
    --to-latest, fixing a typo from the history-breaking 3abf60a31e (setup
    oc repo, 2019-06-17).

--- pkg/cli/admin/upgrade/upgrade.go
+++ pkg/cli/admin/upgrade/upgrade.go
@@ -77,8 +77,8 @@ func New(f kcmdutil.Factory, parentName string, streams genericclioptions.IOStre
 	}
 	flags := cmd.Flags()
 	flags.StringVar(&o.To, "to", o.To, "Specify the version to upgrade to. The version must be on the list of previous or available updates.")
-	flags.StringVar(&o.ToImage, "to-image", o.ToImage, "Provide a release image to upgrade to. WARNING: This option does not check for upgrade compatibility and may break your cluster.")
-	flags.BoolVar(&o.ToLatestAvailable, "to-latest", o.ToLatestAvailable, "Use the next available version")
+	flags.StringVar(&o.ToImage, "to-image", o.ToImage, "Provide a release image to upgrade to.")
+	flags.BoolVar(&o.ToLatestAvailable, "to-latest", o.ToLatestAvailable, "Use the latest available version.")
 	flags.BoolVar(&o.Clear, "clear", o.Clear, "If an upgrade has been requested but not yet downloaded, cancel the update. This has no effect once the update has started.")
 	flags.BoolVar(&o.Force, "force", o.Force, "Forcefully upgrade the cluster even when upgrade release image validation fails and the cluster is reporting errors.")
 	flags.BoolVar(&o.AllowExplicitUpgrade, "allow-explicit-upgrade", o.AllowExplicitUpgrade, "Upgrade even if the upgrade target is not listed in the available versions list.")
@@ -321,7 +321,7 @@ func (o *Options) Run() error {
 			if c := findCondition(cv.Status.Conditions, configv1.RetrievedUpdates); c != nil && c.Status == configv1.ConditionFalse {
 				fmt.Fprintf(o.ErrOut, "warning: Cannot display available updates:\n  Reason: %s\n  Message: %s\n\n", c.Reason, c.Message)
 			} else {
-				fmt.Fprintf(o.Out, "No updates available. You may force an upgrade to a specific release image, but doing so may not be supported and result in downtime or data loss.\n")
+				fmt.Fprintf(o.Out, "No updates available. You may upgrade to a specific release image with --allow-explicit-upgrade --to-image, but doing so may not be supported and result in downtime or data loss.\n")
 			}
 		}
 
