328a720469d7c3c99bfa508824204f27b0a5cc9e
Maciej Szulik <maszulik@redhat.com>
2020-10-30 17:29:05 +0100

    Bug 1888192: add rsync & log re-tries

--- pkg/cli/admin/mustgather/mustgather.go
+++ pkg/cli/admin/mustgather/mustgather.go
@@ -13,9 +13,8 @@ import (
 	"sync"
 	"time"
 
-	"github.com/openshift/oc/pkg/cli/admin/inspect"
-
 	"github.com/spf13/cobra"
+	"k8s.io/klog/v2"
 
 	corev1 "k8s.io/api/core/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
@@ -33,12 +32,12 @@ import (
 	"k8s.io/kubectl/pkg/scheme"
 	"k8s.io/kubectl/pkg/util/templates"
 
-	imagereference "github.com/openshift/library-go/pkg/image/reference"
-
 	imagev1client "github.com/openshift/client-go/image/clientset/versioned/typed/image/v1"
 	"github.com/openshift/library-go/pkg/image/imageutil"
+	imagereference "github.com/openshift/library-go/pkg/image/reference"
 	"github.com/openshift/library-go/pkg/operator/resource/retry"
 
+	"github.com/openshift/oc/pkg/cli/admin/inspect"
 	"github.com/openshift/oc/pkg/cli/rsync"
 )
 
@@ -394,15 +393,23 @@ func (o *MustGatherOptions) copyFilesFromPod(pod *corev1.Pod) error {
 		Destination:   &rsync.PathSpec{PodName: "", Path: destDir},
 		Client:        o.Client,
 		Config:        o.Config,
+		Compress:      true,
 		RshCmd:        fmt.Sprintf("%s --namespace=%s -c copy", o.RsyncRshCmd, pod.Namespace),
 		IOStreams:     streams,
 	}
 	rsyncOptions.Strategy = rsync.NewDefaultCopyStrategy(rsyncOptions)
-	return rsyncOptions.RunRsync()
+	err := rsyncOptions.RunRsync()
+	if err != nil {
+		klog.V(4).Infof("re-trying rsync after initial failure %v", err)
+		// re-try copying data before letting it go
+		err = rsyncOptions.RunRsync()
+	}
+	return err
 }
 
 func (o *MustGatherOptions) getGatherContainerLogs(pod *corev1.Pod) error {
-	return (&logs.LogsOptions{
+	since2s := int64(2)
+	opts := &logs.LogsOptions{
 		Namespace:   pod.Namespace,
 		ResourceArg: pod.Name,
 		Options: &corev1.PodLogOptions{
@@ -414,7 +421,23 @@ func (o *MustGatherOptions) getGatherContainerLogs(pod *corev1.Pod) error {
 		ConsumeRequestFn: logs.DefaultConsumeRequest,
 		LogsForObject:    polymorphichelpers.LogsForObjectFn,
 		IOStreams:        genericclioptions.IOStreams{Out: newPrefixWriter(o.Out, fmt.Sprintf("[%s] POD", pod.Name))},
-	}).RunLogs()
+	}
+
+	for {
+		// gather script might take longer than the default API server time,
+		// so we should check if the gather script still runs and re-run logs
+		// thus we run this in a loop
+		if err := opts.RunLogs(); err != nil {
+			return err
+		}
+
+		// to ensure we don't print all of history set since to past 2 seconds
+		opts.Options.(*corev1.PodLogOptions).SinceSeconds = &since2s
+		if done, _ := o.isGatherDone(pod); done {
+			return nil
+		}
+		klog.V(4).Infof("lost logs, re-trying...")
+	}
 }
 
 func newPrefixWriter(out io.Writer, prefix string) io.Writer {
@@ -429,40 +452,40 @@ func newPrefixWriter(out io.Writer, prefix string) io.Writer {
 }
 
 func (o *MustGatherOptions) waitForGatherToComplete(pod *corev1.Pod) error {
-	err := wait.PollImmediate(10*time.Second, time.Duration(o.Timeout)*time.Second, func() (bool, error) {
-		var err error
-		if pod, err = o.Client.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{}); err != nil {
-			// at this stage pod should exist, we've been gathering container logs, so error if not found
-			if kerrors.IsNotFound(err) {
-				return true, err
-			}
-			return false, nil
-		}
-		var state *corev1.ContainerState
-		for _, cstate := range pod.Status.ContainerStatuses {
-			if cstate.Name == "gather" {
-				state = &cstate.State
-				break
-			}
-		}
+	return wait.PollImmediate(10*time.Second, time.Duration(o.Timeout)*time.Second, func() (bool, error) {
+		return o.isGatherDone(pod)
+	})
+}
 
-		// missing status for gather container => timeout in the worst case
-		if state == nil {
-			return false, nil
-		}
-
-		if state.Terminated != nil {
-			if state.Terminated.ExitCode == 0 {
-				return true, nil
-			}
-			return true, fmt.Errorf("%s/%s unexpectedly terminated: exit code: %v, reason: %s, message: %s", pod.Namespace, pod.Name, state.Terminated.ExitCode, state.Terminated.Reason, state.Terminated.Message)
+func (o *MustGatherOptions) isGatherDone(pod *corev1.Pod) (bool, error) {
+	var err error
+	if pod, err = o.Client.CoreV1().Pods(pod.Namespace).Get(context.TODO(), pod.Name, metav1.GetOptions{}); err != nil {
+		// at this stage pod should exist, we've been gathering container logs, so error if not found
+		if kerrors.IsNotFound(err) {
+			return true, err
 		}
 		return false, nil
-	})
-	if err != nil {
-		return err
 	}
-	return nil
+	var state *corev1.ContainerState
+	for _, cstate := range pod.Status.ContainerStatuses {
+		if cstate.Name == "gather" {
+			state = &cstate.State
+			break
+		}
+	}
+
+	// missing status for gather container => timeout in the worst case
+	if state == nil {
+		return false, nil
+	}
+
+	if state.Terminated != nil {
+		if state.Terminated.ExitCode == 0 {
+			return true, nil
+		}
+		return true, fmt.Errorf("%s/%s unexpectedly terminated: exit code: %v, reason: %s, message: %s", pod.Namespace, pod.Name, state.Terminated.ExitCode, state.Terminated.Reason, state.Terminated.Message)
+	}
+	return false, nil
 }
 
 func (o *MustGatherOptions) waitForGatherContainerRunning(pod *corev1.Pod) error {
