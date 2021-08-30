a00fb88b6854e38410270ca8a9c72855b4333d81
Ross Peoples <rpeoples@redhat.com>
2021-08-30 08:55:10 -0500

    Hardcode paths for tests

--- pkg/cli/image/imagesource/file_test.go
+++ pkg/cli/image/imagesource/file_test.go
@@ -1,17 +1,16 @@
 package imagesource
 
 import (
-	"path/filepath"
 	"runtime"
 	"testing"
 )
 
 func TestGenerateDigestPath(t *testing.T) {
 	path := generateDigestPath("sha256:123456", "mnt", "mirror")
-	desiredPath := filepath.Join("mnt", "mirror", "sha256:123456")
+	desiredPath := "mnt/mirror/sha256:123456"
 
 	if runtime.GOOS == "windows" {
-		desiredPath = filepath.Join("mnt", "mirror", "sha256-123456")
+		desiredPath = "mnt\\mirror\\sha256-123456"
 	}
 
 	if path != desiredPath {
