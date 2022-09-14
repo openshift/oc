1f603bfc747b5917153777b925fefd90abb18a10
Qi Wang <qiwan@redhat.com>
2022-09-13 22:06:39 -0400

    add migrate icsp command
    According to the discussion [resolution](https://docs.google.com/document/d/13h6IJn8wlzXdiPMvCWlMEHOXXqEZ9_GYOl02Wldb3z8/edit?usp=sharing), having a tool in oc to convert icsp file is helpul to land new CRD imagedigestmirrorset. This pr propose command `oc image convert` to convert passed in icsp file to idms file.
    
    ```
    $ ./oc adm migrate icsp --help
    Update imagecontentsourcepolicy file(s) to imagedigestmirrorset file(s). If --dest-dir is unset, the
    imagedigestmirrorset file(s) that can be added to a cluster will be written to file(s) under the current directory.
    
    Examples:
      # update the imagecontentsourcepolicy.yaml to new imagedigestmirrorset file under directory mydir
      oc adm migrate icsp imagecontentsourcepolicy.yaml --dest-dir mydir
    
    Options:
        --dest-dir='':
            Set a specific directory on the local machine to write imagedigestmirrorset file(s) to.
    
    Usage:
      oc adm migrate icsp [flags] [options]
    
    Use "oc options" for a list of global command-line options (applies to all commands).
    
    $ ./oc adm migrate icsp icsp.yml icsp1.yml --dest-dir mydir
    wrote ImageDigestMirrorSet to mydir/imagedigestmirrorset_ubi8repo.1498535234348096290.yaml
    wrote ImageDigestMirrorSet tomydir/imagedigestmirrorset_ubi8repo-1.2106923056265733512.yaml
    
    // example error output
    $ ./oc adm migrate icsp icsp.yml icsp-1.yml icsp-2.yml
    wrote ImageDigestMirrorSet to /home/qiwan/go/src/github.com/openshift/oc/imagedigestmirrorset_ubi8repo.748345346019788996.yaml
    unable to read ImageContentSourceFile icsp-1.yml: open icsp-1.yml: no such file or directory
    unable to read ImageContentSourceFile icsp-2.yml: open icsp-2.yml: no such file or directory
    ```
    
    - ImageDigestMirrorSet implemented: https://github.com/openshift/machine-config-operator/pull/3037
    - ImageDigestMirrorSet epic: https://issues.redhat.com/browse/OCPNODE-521
    
    Signed-off-by: Qi Wang <qiwan@redhat.com>

---
+++ pkg/cli/admin/migrate/icsp/icsp_test.go
@@ -0,0 +1,69 @@
+package icsp
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	operatorv1alpha1 "github.com/openshift/api/operator/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestGenerateIDMS(t *testing.T) {
+	for _, tc := range []struct {
+		name string
+		icsp operatorv1alpha1.ImageContentSourcePolicy
+		want []byte
+	}{
+		{
+
+			name: "convert icsp to idms",
+			icsp: operatorv1alpha1.ImageContentSourcePolicy{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "convert",
+					Labels:      map[string]string{"icspIdx": "1"},
+					Annotations: map[string]string{"icspCon": "idms"},
+				},
+				Spec: operatorv1alpha1.ImageContentSourcePolicySpec{
+					RepositoryDigestMirrors: []operatorv1alpha1.RepositoryDigestMirrors{
+						{Source: "source.example.com", Mirrors: []string{"z1.example.com", "y2.example.com", "x3.example.com"}},
+						{Source: "source.example.net", Mirrors: []string{"z1.example.net", "y2.example.net", "x3.example.net"}},
+					},
+				},
+			},
+			want: []byte(
+				`apiVersion: config.openshift.io/v1
+kind: ImageDigestMirrorSet
+metadata:
+    annotations:
+        icspCon: idms
+    labels:
+        icspIdx: "1"
+    name: convert
+spec:
+    imageDigestMirrors:
+        - mirrors:
+            - z1.example.com
+            - y2.example.com
+            - x3.example.com
+          source: source.example.com
+        - mirrors:
+            - z1.example.net
+            - y2.example.net
+            - x3.example.net
+          source: source.example.net
+`,
+			),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := generateIDMS(tc.icsp)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if !reflect.DeepEqual(result, tc.want) {
+				t.Errorf("generateIDMS() got = %v, want %v, diff = %v", string(result), string(tc.want), cmp.Diff(result, tc.want))
+			}
+		})
+	}
+}
