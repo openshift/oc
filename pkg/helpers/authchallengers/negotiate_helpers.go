3867a82d7849c80c31098a4c8437d0670f07f6dc
Stanislav Laznicka <slaznick@redhat.com>
2023-04-19 15:52:11 +0200

    remove unused tokencmd, move negotiate handlers to their own package

--- pkg/helpers/authchallengers/negotiate_helpers.go
+++ pkg/helpers/authchallengers/negotiate_helpers.go
@@ -1,4 +1,4 @@
-package tokencmd
+package authchallengers
 
 import (
 	"errors"
--- pkg/helpers/authchallengers/negotiator_sspi.go
+++ pkg/helpers/authchallengers/negotiator_sspi.go
@@ -1,7 +1,7 @@
 //go:build windows
 // +build windows
 
-package tokencmd
+package authchallengers
 
 import (
 	"fmt"
