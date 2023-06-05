b451ef684b2bbbbfeaeeb24f40615037cf340e6d
David Eads <deads@redhat.com>
2023-06-05 17:30:26 -0400

    add command to rotate signing certificates

---
+++ pkg/cli/admin/ocpcertificates/ocp_certificates.go
@@ -0,0 +1,32 @@
+package ocpcertificates
+
+import (
+	"fmt"
+
+	"github.com/openshift/oc/pkg/cli/admin/ocpcertificates/regeneratesigners"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
+	ktemplates "k8s.io/kubectl/pkg/util/templates"
+)
+
+var ocpCertificatesLong = ktemplates.LongDesc(`
+	OCP Certificate Commands
+
+	Actions for managing OpenShift platform certificates are exposed here.`)
+
+func NewCommandOCPCertificates(f kcmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	// Main command
+	cmds := &cobra.Command{
+		Use:   "ocp-certificates",
+		Short: "Tools for managing a cluster's certificates",
+		Long:  fmt.Sprintf(ocpCertificatesLong),
+		Run:   kcmdutil.DefaultSubCommandRun(streams.ErrOut),
+	}
+
+	cmds.AddCommand(
+		regeneratesigners.NewCmdRegenerateSigners(f, streams),
+	)
+
+	return cmds
+}
