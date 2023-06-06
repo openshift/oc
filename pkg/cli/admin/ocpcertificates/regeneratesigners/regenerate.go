8d11f867f6449ed63ca649a5ffa2b4c46f29e36f
David Eads <deads@redhat.com>
2023-06-06 09:41:57 -0400

    add a filter to only regenerate those certs that were valid before a particular datetime

--- pkg/cli/admin/ocpcertificates/regeneratesigners/regenerate.go
+++ pkg/cli/admin/ocpcertificates/regeneratesigners/regenerate.go
@@ -3,25 +3,20 @@ package regeneratesigners
 import (
 	"context"
 	"fmt"
-
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	applycorev1 "k8s.io/client-go/applyconfigurations/core/v1"
-	"k8s.io/client-go/kubernetes"
-
-	"github.com/openshift/oc/pkg/cli/admin/ocpcertificates/certgraphanalysis"
-
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-
-	"k8s.io/apimachinery/pkg/runtime"
-
-	corev1 "k8s.io/api/core/v1"
+	"time"
 
 	"github.com/openshift/library-go/pkg/operator/certrotation"
+	"github.com/openshift/oc/pkg/cli/admin/ocpcertificates/certgraphanalysis"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 	"k8s.io/cli-runtime/pkg/printers"
 	"k8s.io/cli-runtime/pkg/resource"
+	applycorev1 "k8s.io/client-go/applyconfigurations/core/v1"
+	"k8s.io/client-go/kubernetes"
 )
 
 const (
@@ -36,7 +31,8 @@ type RegenerateSignersRuntime struct {
 	ResourceFinder genericclioptions.ResourceFinder
 	KubeClient     kubernetes.Interface
 
-	DryRun bool
+	ValidBefore *time.Time
+	DryRun      bool
 
 	Printer printers.ResourcePrinter
 	genericclioptions.IOStreams
@@ -77,7 +73,8 @@ func (o *RegenerateSignersRuntime) forceRegenerationOnResourceInfo(info *resourc
 
 // split here for convenience of unit testing
 func (o *RegenerateSignersRuntime) forceRegenerationOnSecret(secret *corev1.Secret) error {
-	if len(secret.Annotations[certrotation.CertificateIssuer]) == 0 {
+	if len(secret.Annotations[certrotation.CertificateIssuer]) == 0 ||
+		len(secret.Annotations[certrotation.CertificateNotBeforeAnnotation]) == 0 {
 		// TODO this should return an error if the name was specified.
 		// otherwise, not for this command.
 		return nil
@@ -101,6 +98,16 @@ func (o *RegenerateSignersRuntime) forceRegenerationOnSecret(secret *corev1.Secr
 		// not for this command, we only want self-signed signers.
 		//fmt.Printf("#### SKIPPING ns/%v secret/%v issuer=%v\n", secret.Namespace, secret.Name, keyPairInfo.Spec.CertMetadata.CertIdentifier.Issuer)
 		return nil
+	}
+	if o.ValidBefore != nil {
+		notBefore, err := time.Parse(time.RFC3339, secret.Annotations[certrotation.CertificateNotBeforeAnnotation])
+		if err != nil {
+			return fmt.Errorf("error parsing notBefore: %w", err)
+		}
+		if notBefore.After(*o.ValidBefore) {
+			// not for us
+			return nil
+		}
 	}
 
 	applyOptions := metav1.ApplyOptions{
