4b10aaa719139d0ee8a270d63fceea0883f83c11
David Eads <deads@redhat.com>
2023-06-08 16:36:28 -0400

    add command to produce an updated CA bundle for trusting the kube-apiserver

---
+++ pkg/cli/kubectlwrappers/ocp_config_command.go
@@ -0,0 +1,22 @@
+package kubectlwrappers
+
+import (
+	"github.com/openshift/oc/pkg/cli/config/refreshcabundle"
+	cmdutil "github.com/openshift/oc/pkg/helpers/cmd"
+	"github.com/spf13/cobra"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	kclientcmd "k8s.io/client-go/tools/clientcmd"
+	"k8s.io/kubectl/pkg/cmd/config"
+	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/templates"
+)
+
+// NewCmdConfig is a wrapper for the Kubernetes cli config command
+func NewCmdConfig(f kcmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	pathOptions := kclientcmd.NewDefaultPathOptions()
+
+	configCommand := config.NewCmdConfig(pathOptions, streams)
+	configCommand.AddCommand(refreshcabundle.NewCmdConfigRefreshCABundle(f, pathOptions, streams))
+
+	return cmdutil.ReplaceCommandName("kubectl", "oc", templates.Normalize(configCommand))
+}
