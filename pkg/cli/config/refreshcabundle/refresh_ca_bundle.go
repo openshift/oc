348af3d2748341896ec46fc8a962caba93e7268e
David Eads <deads@redhat.com>
2023-06-09 10:27:32 -0400

    refactor to better match kubectl best practice

--- pkg/cli/config/refreshcabundle/refresh_ca_bundle.go
+++ pkg/cli/config/refreshcabundle/refresh_ca_bundle.go
@@ -63,7 +63,10 @@ func NewCmdConfigRefreshCABundle(restClientGetter genericclioptions.RESTClientGe
 		Example:               setClusterExample,
 		Run: func(cmd *cobra.Command, args []string) {
 			cmdutil.CheckErr(o.Complete(cmd))
-			cmdutil.CheckErr(o.Run(context.TODO()))
+			cmdutil.CheckErr(o.Validate())
+			r, err := o.ToRuntime()
+			cmdutil.CheckErr(err)
+			cmdutil.CheckErr(r.Run(context.TODO()))
 
 		},
 	}
@@ -73,22 +76,62 @@ func NewCmdConfigRefreshCABundle(restClientGetter genericclioptions.RESTClientGe
 	return cmd
 }
 
-func (o RefreshCABundleOptions) Run(ctx context.Context) error {
-	err := o.Validate()
-	if err != nil {
-		return err
+func (o *RefreshCABundleOptions) Complete(cmd *cobra.Command) error {
+	args := cmd.Flags().Args()
+	if len(args) > 1 {
+		return helpErrorf(cmd, "Unexpected args: %v", args)
 	}
 
+	if len(args) == 1 {
+		o.ClusterName = args[0]
+	}
+
+	return nil
+}
+
+func (o RefreshCABundleOptions) Validate() error {
+	return nil
+}
+
+func (o *RefreshCABundleOptions) ToRuntime() (*RefreshCABundleRuntime, error) {
 	clientConfig, err := o.RESTClientGetter.ToRESTConfig()
 	if err != nil {
-		return err
+		return nil, err
 	}
 	kubeClient, err := kubernetes.NewForConfig(clientConfig)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	kubeAPIServerCABundleConfigMap, err := kubeClient.CoreV1().ConfigMaps("openshift-kube-controller-manager").Get(ctx, "serviceaccount-ca", metav1.GetOptions{})
+	ret := &RefreshCABundleRuntime{
+		KubeClient:                    kubeClient,
+		ConfigAccess:                  o.ConfigAccess,
+		ClusterName:                   o.ClusterName,
+		DryRun:                        o.DryRun,
+		IOStreams:                     o.IOStreams,
+		OriginalInsecureSkipTLSVerify: clientConfig.Insecure,
+	}
+
+	return ret, nil
+}
+
+type RefreshCABundleRuntime struct {
+	KubeClient kubernetes.Interface
+
+	ConfigAccess clientcmd.ConfigAccess
+	ClusterName  string
+
+	OriginalInsecureSkipTLSVerify bool
+
+	// for convenience. You can compare to an original by doing.
+	// oc config view --raw -o jsonpath='{.clusters[?(@.name == "the-name")].cluster.certificate-authority-data}' | base64 -d
+	DryRun bool
+
+	genericclioptions.IOStreams
+}
+
+func (r *RefreshCABundleRuntime) Run(ctx context.Context) error {
+	kubeAPIServerCABundleConfigMap, err := r.KubeClient.CoreV1().ConfigMaps("openshift-kube-controller-manager").Get(ctx, "serviceaccount-ca", metav1.GetOptions{})
 	if err != nil {
 		return fmt.Errorf("unable to read the CA bundle from the cluster: %w", err)
 	}
@@ -97,47 +140,54 @@ func (o RefreshCABundleOptions) Run(ctx context.Context) error {
 		return fmt.Errorf("cluster somehow missing the CA bundle: not an OCP cluster?")
 	}
 
-	if o.DryRun {
-		fmt.Fprintf(o.Out, caBundle)
+	if r.DryRun {
+		fmt.Fprintf(r.Out, caBundle)
 		return nil
 	}
 
-	config, err := o.ConfigAccess.GetStartingConfig()
+	config, err := r.ConfigAccess.GetStartingConfig()
 	if err != nil {
 		return err
 	}
 
 	// if we have no name specified, then choose the current
-	if len(o.ClusterName) == 0 {
+	if len(r.ClusterName) == 0 {
 		currContext, ok := config.Contexts[config.CurrentContext]
 		if !ok {
 			return fmt.Errorf("cannot determine which context to update")
 		}
-		o.ClusterName = currContext.Cluster
+		r.ClusterName = currContext.Cluster
 	}
 
-	startingStanza, exists := config.Clusters[o.ClusterName]
+	startingStanza, exists := config.Clusters[r.ClusterName]
 	if !exists {
 		return fmt.Errorf("cannot determine which context to update")
 	}
 
-	cluster, err := o.modifyCluster(*startingStanza, caBundle)
+	cluster, err := r.modifyCluster(*startingStanza, caBundle)
 	if err != nil {
 		return fmt.Errorf("failed to update CA bundle: %w", err)
 	}
-	config.Clusters[o.ClusterName] = cluster
+	config.Clusters[r.ClusterName] = cluster
 
-	if err := clientcmd.ModifyConfig(o.ConfigAccess, *config, true); err != nil {
+	if err := clientcmd.ModifyConfig(r.ConfigAccess, *config, true); err != nil {
 		return err
 	}
 
-	fmt.Fprintf(o.Out, "CA bundle for cluster %q updated.\n", o.ClusterName)
+	fmt.Fprintf(r.Out, "CA bundle for cluster %q updated.\n", r.ClusterName)
 
 	return nil
 }
 
-func (o *RefreshCABundleOptions) modifyCluster(existingCluster clientcmdapi.Cluster, caBundle string) (*clientcmdapi.Cluster, error) {
+func (r *RefreshCABundleRuntime) modifyCluster(existingCluster clientcmdapi.Cluster, caBundle string) (*clientcmdapi.Cluster, error) {
 	modifiedCluster := existingCluster
+
+	// if we don't have CA information and we are not using insecure, then we are relying on the system certs
+	connectionReliesOnSystemCertificates := len(modifiedCluster.CertificateAuthorityData) == 0 && len(modifiedCluster.CertificateAuthority) == 0 && !r.OriginalInsecureSkipTLSVerify
+	if connectionReliesOnSystemCertificates {
+		// if we are relying on system certs, we must not rewrite the CA bundle
+		return nil, fmt.Errorf("using system CA bundle to verify server, not allowing refresh to overwrite")
+	}
 
 	if len(modifiedCluster.CertificateAuthorityData) > 0 {
 		modifiedCluster.CertificateAuthorityData = []byte(caBundle)
@@ -154,24 +204,6 @@ func (o *RefreshCABundleOptions) modifyCluster(existingCluster clientcmdapi.Clus
 	}
 
 	return &modifiedCluster, nil
-}
-
-func (o *RefreshCABundleOptions) Complete(cmd *cobra.Command) error {
-	args := cmd.Flags().Args()
-	if len(args) > 1 {
-		return helpErrorf(cmd, "Unexpected args: %v", args)
-	}
-
-	if len(args) == 0 {
-		return nil
-	}
-
-	o.ClusterName = args[0]
-	return nil
-}
-
-func (o RefreshCABundleOptions) Validate() error {
-	return nil
 }
 
 func helpErrorf(cmd *cobra.Command, format string, args ...interface{}) error {
