e0c32c97d7733d3aaeec2f35041450007822b474
David Eads <deads@redhat.com>
2023-06-13 16:02:40 -0400

    add the option to add a command when restarting kubelets

--- pkg/cli/clusteroperator/restartkubelet/restart_kubelet.go
+++ pkg/cli/clusteroperator/restartkubelet/restart_kubelet.go
@@ -3,6 +3,7 @@ package restartkubelet
 import (
 	"context"
 	_ "embed"
+	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 
@@ -19,16 +20,19 @@ var (
 
 type RestartKubeletRuntime struct {
 	PerNodePodRuntime *pernodepod.PerNodePodRuntime
+
+	CommandWhileKubeletIsOff string
 }
 
 func (r *RestartKubeletRuntime) Run(ctx context.Context) error {
-	return r.PerNodePodRuntime.Run(ctx, nil, createPod)
+	return r.PerNodePodRuntime.Run(ctx, nil, r.createPod)
 }
 
-func createPod(ctx context.Context, namespaceName, nodeName, imagePullSpec string) (*corev1.Pod, error) {
+func (r *RestartKubeletRuntime) createPod(ctx context.Context, namespaceName, nodeName, imagePullSpec string) (*corev1.Pod, error) {
 	restartObj := pod.DeepCopy()
 	restartObj.Namespace = namespaceName
 	restartObj.Spec.NodeName = nodeName
 	restartObj.Spec.Containers[0].Image = imagePullSpec
+	restartObj.Spec.Containers[0].Command[2] = strings.ReplaceAll(restartObj.Spec.Containers[0].Command[2], "# optional command", r.CommandWhileKubeletIsOff)
 	return restartObj, nil
 }
