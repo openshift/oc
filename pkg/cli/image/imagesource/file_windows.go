5e3b372c013e8cafab9be1c32f77effd5e07e2b4
Jan Chaloupka <jchaloup@redhat.com>
2024-02-17 11:26:00 +0100

    oc adm catalog mirror: reconstruct the original digest with ':' for sha256 and sha512 algorithms on Windows

--- pkg/cli/image/imagesource/file_windows.go
+++ pkg/cli/image/imagesource/file_windows.go
@@ -1,6 +1,7 @@
 package imagesource
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 )
@@ -13,4 +14,32 @@ import (
 func generateDigestPath(digest string, elem ...string) string {
 	dgst := strings.ReplaceAll(digest, ":", "-")
 	return filepath.Join(append(elem, dgst)...)
+}
+
+// reconstructDigest replaces ":" with "-" which makes the digest parsing nondeterministic.
+// A digest regex corresponds to `[a-z0-9](?:[.+_-][a-z0-9])*:[a-zA-Z0-9=_-]+`.
+// Making "sha256-sha256:659d863050ebd58ebd2ea4b4e89226b494ba39457932ebf11669763eea3b2ed0" a valid digest.
+// After the ":" replacement the filename gets translated into
+// "sha256-sha256-659d863050ebd58ebd2ea4b4e89226b494ba39457932ebf11669763eea3b2ed0" which has
+// two matching digests:
+// - sha256-sha256:659d863050ebd58ebd2ea4b4e89226b494ba39457932ebf11669763eea3b2ed0
+// - sha256:sha256-659d863050ebd58ebd2ea4b4e89226b494ba39457932ebf11669763eea3b2ed0
+//
+// From https://github.com/opencontainers/image-spec/blob/main/descriptor.md#digests:
+// digest                ::= algorithm ":" encoded
+// algorithm             ::= algorithm-component (algorithm-separator algorithm-component)*
+// algorithm-component   ::= [a-z0-9]+
+// algorithm-separator   ::= [+._-]
+// encoded               ::= [a-zA-Z0-9=_-]+
+//
+// Given only sha256 and sha512 are currently supported (based on the linked descriptor.md#digests)
+// we can parse these two cases and panic for the rest.
+func reconstructDigest(digest string) string {
+	if strings.HasPrefix(digest, "sha256-") {
+		return "sha256:" + strings.TrimPrefix(digest, "sha256-")
+	}
+	if strings.HasPrefix(digest, "sha512-") {
+		return "sha512:" + strings.TrimPrefix(digest, "sha512-")
+	}
+	panic(fmt.Sprintf("Only 'sha256-' and 'sha512-' digest prefixes are supported on Windows. Got %q", digest))
 }
