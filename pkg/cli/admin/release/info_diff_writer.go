6a95f319e3d5ebcbbf7e83beaca6ad789d166647
David Eads <deads@redhat.com>
2024-03-22 14:26:30 -0400

    update featureset changes to table based view

---
+++ pkg/cli/admin/release/info_diff_writer.go
@@ -0,0 +1,66 @@
+package release
+
+import (
+	"fmt"
+	"github.com/openshift/library-go/pkg/features"
+	"github.com/openshift/library-go/pkg/markdown"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"strings"
+)
+
+func produceDiffMarkdown(releaseFeatureDiffInfo *features.ReleaseFeatureDiffInfo) ([]byte, error) {
+	allFeatureSets := releaseFeatureDiffInfo.AllFeatureSets()
+	allFeatureSets.Delete("LatencySensitive") // this was a dead-end featureset we removed.
+	allClusterProfiles := releaseFeatureDiffInfo.AllClusterProfiles()
+
+	cols := []features.ColumnTuple{}
+	md := markdown.NewMarkdown("FeatureGate Diff")
+	md.NextTableColumn()
+	md.Exact("FeatureGate ")
+	for _, featureSet := range sets.List(allFeatureSets) {
+		for _, clusterProfile := range sets.List(allClusterProfiles) {
+			cols = append(cols, features.ColumnTuple{
+				ClusterProfile: clusterProfile,
+				FeatureSet:     featureSet,
+			})
+			md.NextTableColumn()
+			md.Exact(fmt.Sprintf("%v on %v ", featureSet, clusterProfile))
+		}
+	}
+	md.EndTableRow()
+	md.NextTableColumn()
+	md.Exact("------ ")
+	for i := 0; i < len(cols); i++ {
+		md.NextTableColumn()
+		md.Exact("--- ")
+	}
+	md.EndTableRow()
+
+	orderedFeatureGates := releaseFeatureDiffInfo.GetOrderedFeatureGates()
+	for _, featureGate := range orderedFeatureGates {
+		md.NextTableColumn()
+		md.Exact(featureGate)
+		for _, col := range cols {
+			change := releaseFeatureDiffInfo.FeatureInfoFor(col.ClusterProfile, col.FeatureSet).ChangedFeatureGates[featureGate]
+			md.NextTableColumn()
+			switch {
+			case strings.Contains(change, "to Enabled"):
+				md.Exact(fmt.Sprintf("<span style=\"background-color: #519450\">%s</span> ", change))
+			case strings.Contains(change, "as Enabled"):
+				md.Exact(fmt.Sprintf("<span style=\"background-color: #519450\">%s</span> ", change))
+			case strings.Contains(change, "Enabled to Disabled"):
+				md.Exact(fmt.Sprintf("<span style=\"background-color: #a63030\">%s</span> ", change))
+			case strings.Contains(change, "as Disabled"):
+				md.Exact(change)
+			case strings.Contains(change, "to Unconditional"):
+				md.Exact(fmt.Sprintf("<span style=\"background-color: #519450\">%s</span> ", change))
+			case strings.Contains(change, "Not Available"):
+				md.Exact(fmt.Sprintf("<span style=\"background-color: #a63030\">%s</span> ", change))
+			default:
+				md.Exact(change)
+			}
+		}
+		md.EndTableRow()
+	}
+	return md.ExactBytes(), nil
+}
