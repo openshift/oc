3f424dac247b35c094aea4092b31acfd8b34a1f9
Arda Güçlü <aguclu@redhat.com>
2024-09-23 10:17:59 +0300

    Skip inspection of the daemonset pods running on data plane

--- pkg/cli/admin/inspect/namespace.go
+++ pkg/cli/admin/inspect/namespace.go
@@ -9,6 +9,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/errors"
@@ -76,10 +77,26 @@ func (o *InspectOptions) gatherNamespaceData(baseDir, namespace string) error {
 	klog.V(1).Infof("    Gathering pod data for namespace %q...\n", namespace)
 	// gather specific pod data
 	if pods := resourcesToStore[corev1.SchemeGroupVersion.WithResource("pods")]; pods != nil {
+		nodeList, err := o.kubeClient.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			errs = append(errs, err)
+		}
+		var nodes []corev1.Node
+		if nodeList.Items != nil {
+			nodes = nodeList.Items
+		}
 		for _, pod := range pods.(*unstructured.UnstructuredList).Items {
-			klog.V(1).Infof("        Gathering data for pod %q\n", pod.GetName())
 			structuredPod := &corev1.Pod{}
 			runtime.DefaultUnstructuredConverter.FromUnstructured(pod.Object, structuredPod)
+			eligible, err := o.podEligibleForCollection(structuredPod, nodes)
+			if err != nil {
+				errs = append(errs, err)
+			}
+			if !eligible {
+				klog.V(1).Infof("        Skipping gathering data for pod %q\n", pod.GetName())
+				continue
+			}
+			klog.V(1).Infof("        Gathering data for pod %q\n", pod.GetName())
 			if err := o.gatherPodData(path.Join(destDir, "/pods/"+pod.GetName()), namespace, structuredPod); err != nil {
 				errs = append(errs, err)
 				continue
@@ -91,4 +108,57 @@ func (o *InspectOptions) gatherNamespaceData(baseDir, namespace string) error {
 		return fmt.Errorf("one or more errors occurred while gathering pod-specific data for namespace: %s\n\n    %v", namespace, errors.NewAggregate(errs))
 	}
 	return nil
+}
+
+// podEligibleForCollection aims to filter out the daemonset pods running on the
+// nodes whose mismatch with the given --node-selector, if data plane node count is greater than 18,
+// in order to save significant storage size and reduces the collection duration.
+func (o *InspectOptions) podEligibleForCollection(pod *corev1.Pod, nodes []corev1.Node) (bool, error) {
+	isDaemonSet := false
+	if len(pod.OwnerReferences) > 0 {
+		for _, OwnerRef := range pod.OwnerReferences {
+			if OwnerRef.Kind == "DaemonSet" {
+				isDaemonSet = true
+				break
+			}
+		}
+	}
+	if !isDaemonSet {
+		return true, nil
+	}
+
+	workerNodeCount := 0
+	for _, node := range nodes {
+		if _, ok := node.Labels["node-role.kubernetes.io/control-plane"]; ok {
+			continue
+		}
+		workerNodeCount++
+	}
+	const daemonsetCollectionMaxNodeSize = 18
+	if workerNodeCount <= daemonsetCollectionMaxNodeSize {
+		return true, nil
+	}
+
+	var podNode *corev1.Node
+	for _, node := range nodes {
+		if node.Name != pod.Spec.NodeName {
+			continue
+		}
+
+		podNode = &node
+		break
+	}
+
+	if podNode == nil {
+		return false, nil
+	}
+
+	nodeSelector, err := labels.Parse(o.nodeSelector)
+	if err != nil {
+		return true, err
+	}
+	if nodeSelector.Matches(labels.Set(podNode.Labels)) {
+		return true, nil
+	}
+	return false, nil
 }
