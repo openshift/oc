e1b0934f1bd8685614909a90f641494633ce421e
Petr Muller <muller@redhat.com>
2025-03-03 20:39:15 +0100

    `upgrade status`: store example fixtures in Go
    ...only temporarily, it will be easier to accomodate API changes this way

--- pkg/cli/admin/upgrade/status/examples_test.go
+++ pkg/cli/admin/upgrade/status/examples_test.go
@@ -10,6 +10,10 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
+
+	updatev1alpha1 "github.com/openshift/api/update/v1alpha1"
 )
 
 func compareWithFixture(t *testing.T, actualOut []byte, usPath string, outputSuffix string) {
@@ -37,6 +41,93 @@ func compareWithFixture(t *testing.T, actualOut []byte, usPath string, outputSuf
 	}
 }
 
+var cvUpdatingFalse = metav1.Condition{
+	Type:   string(updatev1alpha1.ClusterVersionStatusInsightUpdating),
+	Status: metav1.ConditionFalse,
+	Reason: string(updatev1alpha1.ClusterVersionNotProgressing),
+}
+
+var mcpUpdatingFalse = metav1.Condition{
+	Type:   MachineConfigPoolStatusInsightUpdating,
+	Status: metav1.ConditionFalse,
+	Reason: MachineConfigPoolStatusInsightUpdatingReasonCompleted,
+}
+
+var fixtures = map[string]updatev1alpha1.UpdateStatusStatus{
+	"examples/not-upgrading-us.yaml": {
+		ControlPlane: updatev1alpha1.ControlPlane{
+			Informers: []updatev1alpha1.ControlPlaneInformer{
+				{
+					Name: "cpi",
+					Insights: []updatev1alpha1.ControlPlaneInsight{
+						{
+							UID: "cv-version",
+							ControlPlaneInsightUnion: updatev1alpha1.ControlPlaneInsightUnion{
+								Type: updatev1alpha1.ClusterVersionStatusInsightType,
+								ClusterVersionStatusInsight: &updatev1alpha1.ClusterVersionStatusInsight{
+									Conditions: []metav1.Condition{cvUpdatingFalse},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		WorkerPools: []updatev1alpha1.Pool{
+			{
+				Name: "worker",
+				Informers: []updatev1alpha1.WorkerPoolInformer{
+					{
+						Name: "nodes",
+						Insights: []updatev1alpha1.WorkerPoolInsight{
+							{
+								UID: "mcp-worker-status",
+								WorkerPoolInsightUnion: updatev1alpha1.WorkerPoolInsightUnion{
+									Type: updatev1alpha1.MachineConfigPoolStatusInsightType,
+									MachineConfigPoolStatusInsight: &updatev1alpha1.MachineConfigPoolStatusInsight{
+										Conditions: []metav1.Condition{mcpUpdatingFalse},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
+func updateFixtureInput(t *testing.T, path string) {
+	t.Helper()
+	fixtureStatus, ok := fixtures[path]
+	if !ok {
+		return
+	}
+
+	fixture := updatev1alpha1.UpdateStatus{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "UpdateStatus",
+			APIVersion: "update.openshift.io/v1alpha1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "cluster",
+		},
+		Status: fixtureStatus,
+	}
+
+	fixtureFile, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		t.Fatalf("Error when opening fixture: %v", err)
+	}
+	defer fixtureFile.Close()
+
+	jsonSerializer := json.NewSerializerWithOptions(json.DefaultMetaFactory, nil, nil, json.SerializerOptions{Yaml: true, Pretty: true})
+	err = jsonSerializer.Encode(&fixture, fixtureFile)
+	if err != nil {
+		t.Fatalf("Error when encoding fixture: %v", err)
+	}
+}
+
 func TestExamples(t *testing.T) {
 	updateStatuses, err := filepath.Glob("examples/*-us.yaml")
 	if err != nil {
@@ -61,6 +152,7 @@ func TestExamples(t *testing.T) {
 	}
 
 	for _, us := range updateStatuses {
+		updateFixtureInput(t, us)
 		for _, variant := range variants {
 			variant := variant
 			t.Run(fmt.Sprintf("%s-%s", us, variant.name), func(t *testing.T) {
