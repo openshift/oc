293a1e78cffd720274798832de8c3eafbb5c0975
W. Trevor King <wking@tremily.us>
2025-05-06 17:05:24 -0700

    pkg/cli/admin/upgrade/recommend: New, feature-gated --accepted-risk
    [1] is suggesting ClusterVersion API changes to make it easier for
    cluster administrators to manage many clusters and updates vs. a more
    limited set of update risks.  One example scenario:
    
    1. A cluster admin gets conditional updates of a cluster and evaluates
       the risks in the conditional updates.
    2. After evaluation, the admin decides that some risks can be accepted
       on all or some of the managed clusters.
    3. The administrator runs the 'recommend' command with
       '--accepted-risk A,B,C --version 4.y.z' to see if any other risks
       turn up for that update on that cluster.  Nonzero exit code says
       more investigation is needed, some subset of which will be
       "additional risk D to assess".
    
    Besides making life easier for admins with lots of clusters
    considering one A->B update, this option will make life easier for
    admins with one cluster that updates multiple hops all exposed to a
    single risk (e.g. if PodDisruptionBudgetAtLimit becomes a general
    risk, but the admin never wants to resolve that pre-update, they can
    accept it for multiple updates in a single cluster).
    
    The exit code handling is only tied to --version.
    
    When listing multiple update target options, the admin is asking for
    help in chosing an update target, and that cannot be bundled up into
    an exit code; they'll have to read the report to get enough context to
    decide.  And there may be some that are acceptable and some that have
    unaccepted risks, and trying to bundle that all up into a single exit
    code could go a few ways (exit non-zero if any choice has unaccepted
    risks?  Exit 0 if any choice is acceptable, even if other choices have
    unaccepted risks?), and it seems like folks could misinterpret
    whichever approach we took and be surprised.  Exiting zero on "we
    succesfully compiled this report" forces the admin to read the report
    to extract any value, and that reduces the chance that they
    misinterpret what we were trying to say.
    
    [1]: https://issues.redhat.com/browse/OCPSTRAT-2118

--- pkg/cli/admin/upgrade/recommend/recommend.go
+++ pkg/cli/admin/upgrade/recommend/recommend.go
@@ -14,6 +14,7 @@ import (
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/cli-runtime/pkg/genericiooptions"
 	"k8s.io/client-go/rest"
 	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
@@ -61,6 +62,10 @@ func New(f kcmdutil.Factory, streams genericiooptions.IOStreams) *cobra.Command 
 	flags.BoolVar(&o.showOutdatedReleases, "show-outdated-releases", o.showOutdatedReleases, "Display additional older releases.  These releases may be exposed to known issues which have been fixed in more recent releases.  But all updates will contain fixes not present in your current release.")
 	flags.StringVar(&o.rawVersion, "version", o.rawVersion, "Select a particular target release to display by version.")
 
+	if kcmdutil.FeatureGate("OC_ENABLE_CMD_UPGRADE_RECOMMEND_ACCEPTED_RISKS").IsEnabled() {
+		flags.StringSliceVar(&o.acceptedRisks, "accepted-risk", o.acceptedRisks, "Comma-delimited names for risks that you find acceptable.  With --version, any unaccepted risks will result in a non-zero exit code.")
+	}
+
 	// TODO: We can remove this flag once the idea about `oc adm upgrade recommend` stabilizes and the command
 	//       is promoted out of the OC_ENABLE_CMD_UPGRADE_RECOMMEND feature gate
 	flags.StringVar(&o.mockData.cvPath, "mock-clusterversion", "", "Path to a YAML ClusterVersion object to use for testing (will be removed later).")
@@ -80,6 +85,9 @@ type options struct {
 
 	// version is the parsed form of rawVersion.  Consumers after options.Complete should prefer this property.
 	version *semver.Version
+
+	// acceptedRisks is a slice of accepted risk slugs.
+	acceptedRisks []string
 
 	RESTConfig *rest.Config
 	Client     configv1client.Interface
@@ -125,6 +133,9 @@ func (o *options) Complete(f kcmdutil.Factory, cmd *cobra.Command, args []string
 }
 
 func (o *options) Run(ctx context.Context) error {
+	risks := sets.New[string]()
+	acceptedRisks := sets.New[string](o.acceptedRisks...)
+
 	var cv *configv1.ClusterVersion
 	if cv = o.mockData.clusterVersion; cv == nil {
 		var err error
@@ -139,33 +150,59 @@ func (o *options) Run(ctx context.Context) error {
 
 	if c := findClusterOperatorStatusCondition(cv.Status.Conditions, clusterStatusFailing); c != nil {
 		if c.Status != configv1.ConditionFalse {
-			fmt.Fprintf(o.Out, "%s=%s:\n\n  Reason: %s\n  Message: %s\n\n", c.Type, c.Status, c.Reason, strings.ReplaceAll(c.Message, "\n", "\n  "))
+			context := ""
+			if acceptedRisks.Has(string(clusterStatusFailing)) {
+				context = "accepted "
+			}
+			fmt.Fprintf(o.Out, "%s%s=%s:\n\n  Reason: %s\n  Message: %s\n\n", context, c.Type, c.Status, c.Reason, strings.ReplaceAll(c.Message, "\n", "\n  "))
+			risks.Insert(string(clusterStatusFailing))
 		}
 	} else {
-		fmt.Fprintf(o.ErrOut, "warning: No current %s info, see `oc describe clusterversion` for more details.\n", clusterStatusFailing)
+		context := ""
+		if acceptedRisks.Has(string(clusterStatusFailing)) {
+			context = "accepted "
+		}
+		fmt.Fprintf(o.ErrOut, "%swarning: No current %s info, see `oc describe clusterversion` for more details.\n", context, clusterStatusFailing)
+		risks.Insert(string(clusterStatusFailing))
 	}
 
 	if c := findClusterOperatorStatusCondition(cv.Status.Conditions, configv1.OperatorProgressing); c != nil && c.Status == configv1.ConditionTrue && len(c.Message) > 0 {
-		fmt.Fprintf(o.Out, "info: An update is in progress.  You may wish to let this update complete before requesting a new update.\n  %s\n\n", strings.ReplaceAll(c.Message, "\n", "\n  "))
+		context := ""
+		if acceptedRisks.Has(string(configv1.OperatorProgressing)) {
+			context = "accepted "
+		}
+		fmt.Fprintf(o.Out, "%sinfo: An update is in progress.  You may wish to let this update complete before requesting a new update.\n  %s\n\n", context, strings.ReplaceAll(c.Message, "\n", "\n  "))
+		risks.Insert(string(configv1.OperatorProgressing))
 	}
 
 	if o.precheckEnabled {
 		conditions, err := o.precheck(ctx)
 		if err != nil {
 			fmt.Fprintf(o.Out, "Failed to check for at least some preconditions: %v\n", err)
+			risks.Insert("FailedToCompletePrecheck")
 		}
 		var happyConditions []string
+		var acceptedConditions []string
 		var unhappyConditions []string
 		for _, condition := range conditions {
 			if condition.Status == metav1.ConditionTrue {
 				happyConditions = append(happyConditions, fmt.Sprintf("%s (%s)", condition.Type, condition.Reason))
 			} else {
-				unhappyConditions = append(unhappyConditions, condition.Type)
+				risks.Insert(condition.riskAcceptanceName)
+				if acceptedRisks.Has(condition.riskAcceptanceName) {
+					acceptedConditions = append(acceptedConditions, condition.Type)
+				} else {
+					unhappyConditions = append(unhappyConditions, condition.Type)
+				}
 			}
 		}
 		if len(happyConditions) > 0 {
 			sort.Strings(happyConditions)
 			fmt.Fprintf(o.Out, "The following conditions found no cause for concern in updating this cluster to later releases: %s\n\n", strings.Join(happyConditions, ", "))
+		}
+		if len(acceptedConditions) > 0 {
+			sort.Strings(acceptedConditions)
+			fmt.Fprintf(o.Out, "The following conditions found cause for concern in updating this cluster to later releases, but were explicitly accepted via --accept-risk: %s\n\n", strings.Join(acceptedConditions, ", "))
 		}
 		if len(unhappyConditions) > 0 {
 			sort.Strings(unhappyConditions)
@@ -261,7 +298,16 @@ func (o *options) Run(ctx context.Context) error {
 					if c := notRecommendedCondition(update); c == nil {
 						fmt.Fprintf(o.Out, "Update to %s has no known issues relevant to this cluster.\nImage: %s\nRelease URL: %s\n", update.Release.Version, update.Release.Image, update.Release.URL)
 					} else {
-						fmt.Fprintf(o.Out, "Update to %s %s=%s:\nImage: %s\nRelease URL: %s\nReason: %s\nMessage: %s\n", update.Release.Version, c.Type, c.Status, update.Release.Image, update.Release.URL, c.Reason, strings.ReplaceAll(c.Message, "\n", "\n  "))
+						context := ""
+						if acceptedRisks.Has("ConditionalUpdateRisk") {
+							context = "accepted "
+						}
+						fmt.Fprintf(o.Out, "Update to %s %s=%s:\nImage: %s\nRelease URL: %s\nReason: %s%s\nMessage: %s\n", update.Release.Version, c.Type, c.Status, update.Release.Image, update.Release.URL, context, c.Reason, strings.ReplaceAll(c.Message, "\n", "\n  "))
+						risks.Insert("ConditionalUpdateRisk")
+					}
+					unaccepted := risks.Difference(acceptedRisks)
+					if unaccepted.Len() > 0 {
+						return fmt.Errorf("unaccepted risks: %s", strings.Join(sets.List(unaccepted), ","))
 					}
 					return nil
 				}
