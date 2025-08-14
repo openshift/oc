5f1dbb8b1a671ba1ff3f65332e4d4ad60009528f
Hongkai Liu <hongkailiu@users.noreply.github.com>
2025-08-14 17:46:35 -0400

    Move not-lines-related table.Write out of the loop

--- pkg/cli/admin/upgrade/status/controlplane.go
+++ pkg/cli/admin/upgrade/status/controlplane.go
@@ -499,11 +499,11 @@ func (d *controlPlaneStatusDisplayData) Write(f io.Writer, detailed bool, now ti
 				reason = "-"
 			}
 			lines := strings.Split(o.Condition.Message, "\n")
+			_, _ = table.Write([]byte(o.Name + "\t"))
+			_, _ = table.Write([]byte(shortDuration(now.Sub(o.Condition.LastTransitionTime.Time)) + "\t"))
+			_, _ = table.Write([]byte(ellipsis(reason, 20) + "\t"))
 			for i, line := range lines {
 				if i == 0 {
-					_, _ = table.Write([]byte(o.Name + "\t"))
-					_, _ = table.Write([]byte(shortDuration(now.Sub(o.Condition.LastTransitionTime.Time)) + "\t"))
-					_, _ = table.Write([]byte(ellipsis(reason, 20) + "\t"))
 					_, _ = table.Write([]byte(line + "\n"))
 				} else {
 					_, _ = table.Write([]byte("\t\t\t" + line + "\n"))
