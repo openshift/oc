5f5ed85a0daf8a2959e31343cc0fa327c927f9ba
Arda Güçlü <aguclu@redhat.com>
2025-12-16 14:39:57 +0300

    Bump docker/docker to 28.5.2 to drop dependency of containerd/containerd

--- vendor/github.com/docker/docker/pkg/system/init_windows.go
+++ vendor/github.com/docker/docker/pkg/system/init_windows.go
@@ -1,10 +1,10 @@
-package system // import "github.com/docker/docker/pkg/system"
+package system
 
 // containerdRuntimeSupported determines if containerd should be the runtime.
 var containerdRuntimeSupported = false
 
-// InitContainerdRuntime sets whether to use containerd for runtime on Windows.
-func InitContainerdRuntime(cdPath string) {
+// EnableContainerdRuntime sets whether to use containerd for runtime on Windows.
+func EnableContainerdRuntime(cdPath string) {
 	if len(cdPath) > 0 {
 		containerdRuntimeSupported = true
 	}
